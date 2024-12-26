1794077d3fcb3db0ab381ee85dd273b9328cb1fd
Alberto Mur <alberto.mur@doofinder.com>
2024-12-26 02:24:50 +0100

    Add PLT for mix

--- mix.exs
+++ mix.exs
@@ -7,7 +7,8 @@ defmodule AdventOfCode.MixProject do
       version: "0.1.0",
       elixir: "~> 1.16",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      dialyzer: [plt_add_apps: [:mix]]
     ]
   end
 
